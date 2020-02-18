74e39b0b5b90ff6f373553381e726d635b96c67f
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2020-02-18 11:21:34 -0500

    Project structure
    Project structure

---
+++ src/main/java/org/ecuadorjug/reactive/MonolithMigrateToReactiveModulesApplication.java
@@ -0,0 +1,13 @@
+package org.ecuadorjug.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithMigrateToReactiveModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithMigrateToReactiveModulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ecuadorjug/reactive/MonolithMigrateToReactiveModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecuadorjug.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithMigrateToReactiveModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
